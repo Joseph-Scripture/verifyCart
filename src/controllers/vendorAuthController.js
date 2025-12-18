d61e9dc08fb39f03e7d04189d32751026e610a25
Joseph-Scripture <joescript237@gmail.com>
2025-12-18 19:36:37 +0100

    fix:Corrected schema and authController to match PRD

---
+++ src/controllers/vendorAuthController.js
@@ -0,0 +1,68 @@
+import bcrypt from 'bcryptjs';
+import prisma from '../config/db.js';
+import generateToken from '../utils/generateToken.js';
+
+export const vendorSignup = async (req, res) => {
+    const {name, email, password, phone, businessName} = req.body; 
+    if (!name || !email || !password || !phone || !businessName) {
+        return res.status(400).json({ message: 'Please fill in all fields' });
+    }
+    try {
+        const existingVendor = await prisma.vendor.findUnique({
+            where: {
+                email,
+            },
+        });
+        if (existingVendor) {
+            return res.status(400).json({ message: 'Vendor already exists' });
+        };
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const vendor = await prisma.vendor.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+                phone,
+                businessName,
+            },
+        });
+        const token = generateToken({vendorId: vendor.id}, res);
+        return res.status(201).json({
+            success: true,
+            message: 'Vendor created successfully',
+            token,
+        });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+    
+}
+export const vendorLogin = async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({message: 'Please fill in all fields'})
+    }
+    try {
+        const vendor = await prisma.vendor.findUnique({
+            where:{email},
+        });
+        if(!vendor){
+            return res.status(401).json({message: 'Invalid email or password'})
+        }
+        const isMatch = await bcrypt.compare(password, vendor.password);
+        if(!isMatch){
+            return res.status(401).json({message: 'Invalid email or password'})
+        }
+        const token = generateToken({vendorId: vendor.id}, res);
+        return res.status(200).json({
+            success: true,
+            message: 'Vendor logged in successfully',
+            token,
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,17 +1,20 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-const generateToken = (userId, res) => {
-   const payload = {id:userId};
-   const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn:process.env.JWT_EXPIRES_IN || '7d',
-   });
+
+const generateToken = (payload, res) => {
+   const token = jwt.sign(
+      payload, 
+      process.env.JWT_SECRET,
+      {
+         expiresIn: process.env.JWT_EXPIRES_IN || '7d',
+      }
+   );
    res.cookie("jwt", token, {
-    httpOnly:true,
-    secure:process.env.NODE_ENV === 'production',
-    sameSite:'strict',
-    maxAge:7 * 24 * 60 * 60 * 1000,
-    
+   httpOnly:true,
+   secure:process.env.NODE_ENV === 'production',
+   sameSite:'strict',
+   maxAge:7 * 24 * 60 * 60 * 1000,
    })
    return token
 }
-export default generateToken
+export default generateToken;
